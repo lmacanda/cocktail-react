ff9461e971c3233dd834002b6edb8af35749d248
lmacanda <laurina02@hotmail.com>
2022-09-30 17:28:47 +0100

    created LetterA component
    Component in homepage that will render cocktails that start with A

--- src/components/LetterA.jsx
+++ src/components/LetterA.jsx
@@ -1,1 +1,30 @@
+import { useEffect, useState } from "react";
 
+function LetterA() {
+  const [letterA, setLetterA] = useState([]);
+
+  useEffect(() => {
+    getLetterA();
+  }, []);
+
+  const getLetterA = async () => {
+    const apiUrl = "https://thecocktaildb.com/api/json/v1/1/search.php?f=a";
+    const api = await fetch(`${apiUrl}`);
+    const data = await api.json();
+    setLetterA(data.drinks);
+  };
+
+  return (
+    <div>
+      {letterA.map((drink) => {
+        return (
+          <div key={drink.idDrink}>
+            <p>{drink.strDrink}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default LetterA;
